c55012d64fc3c282ba49361dac9cbefa513a4744
JoanPareja <joanparejapinyol@gmail.com>
2017-10-10 19:30:13 +0200

    Mesh component
    Seems done, needs testing when Game Object is implemented in scene

--- 3DEngine/Source/Mesh.cpp
+++ 3DEngine/Source/Mesh.cpp
@@ -1,9 +1,61 @@
 #include "Mesh.h"
+#include "Glew\include\glew.h"
 
 Mesh::Mesh()
 {
 }
 
+Mesh::Mesh(RenderData nrender_data) : render_data(nrender_data)
+{
+	for (uint i = 0; i < render_data.num_vertices * 3; i += 3)
+	{
+		bounding_box.Enclose(vec(render_data.vertices[i], render_data.vertices[i + 1], render_data.vertices[i + 2]));
+		if (i == 0) bounding_box.minPoint = bounding_box.maxPoint;
+	}
+}
+
 Mesh::~Mesh()
 {
+	if (render_data.num_indices > 0)glDeleteBuffers(1, &render_data.id_indices);
+	if (render_data.num_vertices > 0)glDeleteBuffers(1, &render_data.id_vertices);
+	if (render_data.num_normals > 0)glDeleteBuffers(1, &render_data.id_normals);
+	if (render_data.num_tex_vertices > 0)	glDeleteBuffers(1, &render_data.id_tex_vertices);
+}
+
+const RenderData* Mesh::GetRenderData()
+{
+	return &render_data;
+}
+
+void Mesh::Update()
+{
+	Render();
+}
+
+void Mesh::Render()
+{
+	//Enable opengl states
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+	//Bind texture coords buffer
+	glBindBuffer(GL_ARRAY_BUFFER, render_data.id_tex_vertices);
+	glTexCoordPointer(3, GL_FLOAT, 0, NULL);
+
+	//Bind vertex buffer
+	glBindBuffer(GL_ARRAY_BUFFER, render_data.id_vertices);
+	glVertexPointer(3, GL_FLOAT, 0, NULL);
+
+	//Bind and draw with indices
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, render_data.id_indices);
+	glDrawElements(GL_TRIANGLES, render_data.num_indices, GL_UNSIGNED_INT, NULL);
+
+	//Disable opengl states
+	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+	glDisableClientState(GL_VERTEX_ARRAY);
+
+	// Clear Bind buffers
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
 }
--- 3DEngine/Source/Mesh.h
+++ 3DEngine/Source/Mesh.h
@@ -2,12 +2,49 @@
 #define __MESH_H__
 
 #include "Component.h"
+#include "Math.h"
+
+
+struct RenderData
+{
+	uint id_indices = 0; // id in VRAM
+	uint num_indices = 0;
+	uint* indices = nullptr;
+
+	uint id_vertices = 0; // id in VRAM
+	uint num_vertices = 0;
+	float* vertices = nullptr;
+
+	uint id_normals = 0;
+	uint num_normals = 0;
+	float*	normals = nullptr;
+
+	uint id_tex_vertices = 0;
+	uint num_tex_vertices = 0;
+	float* tex_vertices = nullptr;
+
+};
+
 
 class Mesh : public Component
 {
 public:
 	Mesh();
+	Mesh(RenderData render_data);
 	~Mesh();
+
+	const RenderData* GetRenderData();
+
+	void Update();
+
+private:
+	void Render();
+
+public:
+
+	RenderData render_data;
+	math::AABB bounding_box;
+
 
 };
 
