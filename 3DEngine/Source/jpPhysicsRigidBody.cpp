80d33eac16f06dc078d3027e5e092b2aa88c5961
JoanPareja <imbecilustotalus@gmail.com>
2017-12-10 14:14:43 +0100

    Some utility added to jpPhysicsRigidBody

--- 3DEngine/Source/jpPhysicsRigidBody.cpp
+++ 3DEngine/Source/jpPhysicsRigidBody.cpp
@@ -5,17 +5,53 @@ jpPhysicsRigidBody::jpPhysicsRigidBody(physx::PxPhysics* px_physics)
 {
 	//Create a default rigidbody 
 	//default transform == identity
-	//default geometry == sphere with 0.5 radius
+	//default geometry == box with 0.5 halfExtent
 	//default material: 0.5 friction(static and dynamic) and 0.5 restitution
 	physx::PxMaterial* default_material = px_physics->createMaterial(0.5f, 0.5f, 0.5f);
-	px_body = physx::PxCreateDynamic(*px_physics, physx::PxTransform(), physx::PxSphereGeometry(0.5f), *default_material, 1.0f);
+	body_shape = px_physics->createShape(physx::PxBoxGeometry(0.5f,0.5f,0.5f), *default_material);
+	px_body = physx::PxCreateDynamic(*px_physics, physx::PxTransform(), *body_shape, 1.0f);
+
+	//Detach the shape by default,
+	//Shape must be activated using calling a diferent function
+	//Made like this to separet the rigidbody from the collider
+	px_body->detachShape(*body_shape);
 }
 
 jpPhysicsRigidBody::~jpPhysicsRigidBody()
 {
 }
 
+void jpPhysicsRigidBody::ActivateShape()
+{
+	px_body->attachShape(*body_shape);
+}
+
 void jpPhysicsRigidBody::SetGeometry()
 {
+	
+}
 
+void jpPhysicsRigidBody::SetBoxGeometry(float x_scale, float y_scale, float z_scale)
+{
+	physx::PxBoxGeometry boxGeometry;
+	body_shape->getBoxGeometry(boxGeometry);
+	boxGeometry.halfExtents.x = x_scale/2;
+	boxGeometry.halfExtents.y = y_scale/2;
+	boxGeometry.halfExtents.z = z_scale/2;
+}
+
+void jpPhysicsRigidBody::SetMaterial(float static_friction, float dynamic_friction, float restitution)
+{
+	physx::PxMaterial* material;
+	body_shape->getMaterials(&material, 1);
+	material->setStaticFriction(static_friction);
+	material->setDynamicFriction(dynamic_friction);
+	material->setRestitution(restitution);
+}
+
+void jpPhysicsRigidBody::SetShape(physx::PxShape * new_shape)
+{
+	//Might not work
+	px_body->detachShape(*body_shape);
+	body_shape = new_shape;
 }
--- 3DEngine/Source/jpPhysicsRigidBody.h
+++ 3DEngine/Source/jpPhysicsRigidBody.h
@@ -10,12 +10,17 @@ public:
 	jpPhysicsRigidBody(physx::PxPhysics* px_physics);
 	~jpPhysicsRigidBody();
 
-	void SetGeometry();
+	void ActivateShape();
+
+	void SetGeometry(); //DO NOT USE, does nothing, will be later on defined to use with convexmesh
+	void SetBoxGeometry(float x_scale, float y_scale, float z_scale);	//Regular scale, the method will turn it into halfExtents
+	void SetMaterial(float static_friction, float dynamic_friction, float restitution);
+
+	void SetShape(physx::PxShape* new_shape);
 
 private:
 
 	physx::PxRigidDynamic* px_body = nullptr;
-
 	physx::PxShape* body_shape = nullptr;
 };
 
