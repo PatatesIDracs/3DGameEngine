70e52414346c62378c7dd0ef8a82fb9a3f279380
Pere Rifà Planellas <pere.rifaplanellas@gmail.com>
2017-12-13 13:26:36 +0100

    Collider can be modified (size & scale)

--- 3DEngine/Source/RbCollider.cpp
+++ 3DEngine/Source/RbCollider.cpp
@@ -4,15 +4,14 @@
 #include "Application.h"
 #include "ModulePhysics.h"
 
+#include "Transform.h"
+
 #include "jpPhysicsRigidBody.h"
 
 #include "Glew\include\glew.h"
 
 RbCollider::RbCollider(GameObject * parent, bool isactive) : Component(parent, COMP_RBSHAPE, true)
 {
-	if (parent != nullptr)
-		transform = parent->GetTransform();
-
 
 	physics_body = App->physics->GetNewRigidBody(0);
 	//Set as Knimeatic(Static) by default
@@ -32,6 +31,11 @@ void RbCollider::Update()
 
 void RbCollider::UpdateTransform()
 {
+	if ((rigid_body_comp && !rigid_body_comp->own_update) || physics_body) {
+		float3 fpos = transform->GetPosition();
+		Quat quat = transform->GetRotQuat();
+		physics_body->px_body->setGlobalPose(physx::PxTransform(physx::PxVec3(fpos.x, fpos.y, fpos.z), physx::PxQuat(quat.x, quat.y, quat.z, quat.w)));
+	}
 }
 
 void RbCollider::DrawComponent()
@@ -40,7 +44,31 @@ void RbCollider::DrawComponent()
 	Component::DrawComponent();
 	if (ImGui::CollapsingHeader("Rb Collider"))
 	{
+		ImGui::Combo("Collider", (int*)&collider_type, "Box\0Sphere\0Plane\0", 3);
+		if (ImGui::InputFloat3("Position", &position.x, 2, ImGuiInputTextFlags_EnterReturnsTrue) && physics_body) {
+			physx::PxTransform transf =	physics_body->px_body->getGlobalPose();
+			transf.p = physx::PxVec3(position.x, position.y, position.z);
+			physics_body->px_body->setGlobalPose(transf);
+		}
+		switch (collider_type)
+		{
+		case COLL_BOX:
+			if (ImGui::InputFloat3("Size", &size.x, 2, ImGuiInputTextFlags_EnterReturnsTrue))
+				UpdateCollider();
+			break;
+		case COLL_SPHERE:
+			if (ImGui::InputFloat("Radius", &rad, 0.1, 1, 2, ImGuiInputTextFlags_EnterReturnsTrue))
+				UpdateCollider();
+			break;
+		case COLL_PLANE:
+			break;
+		default:
+			break;
+		}
 
+		if (curr_type != collider_type) {
+			if (ImGui::Button("Change Collider"));
+		}
 	}
 	ImGui::PopID();
 }
@@ -55,6 +83,26 @@ void RbCollider::Load(char * cursor, int & bytes_copied)
 
 void RbCollider::GetOwnBufferSize(uint & buffer_size)
 {
+}
+
+void RbCollider::ChangeParent(GameObject * new_parent)
+{
+	Component::ChangeParent(new_parent);
+	LookForRigidBody();
+
+	transform = parent->GetTransform();
+	UpdateTransform();
+}
+
+void RbCollider::ChangeCollider()
+{
+	//TODO: Create Collider of type curr_type
+}
+
+void RbCollider::UpdateCollider()
+{
+	if(physics_body)
+		physics_body->SetShapeScale(physx::PxVec3(size.x, size.y, size.z), rad);
 }
 
 RigidBody * RbCollider::LookForRigidBody()
